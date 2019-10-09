805524610fb6e71682760f840714501506b9923a
Pratik Dixit <43335363+Pratik150196@users.noreply.github.com>
2019-10-10 03:12:35 +0530

    Update urls.py

--- bank_app/app/urls.py
+++ bank_app/app/urls.py
@@ -2,8 +2,9 @@
 from django.urls import path,include
 from rest_framework import routers
 from .views import AppView,HelloView,AppView1
+from django.conf.urls import url
 urlpatterns = [
-path("ifsc/<ifsc>/",AppView.as_view()),#ifsc
-path("name_city/<bank_name>/<city>/",AppView1.as_view()),#bank name and city
+url(r"^ifsc/$",AppView.as_view()),#ifsc
+url(r"^name_city/$",AppView1.as_view()),#bank name and city
 path('hello/',HelloView.as_view(), name='hello'),
 ]
